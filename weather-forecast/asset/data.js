c66b1001f181fef3d928d3c52d57b99c99ba9b58
tthjose <138036385+tthjose@users.noreply.github.com>
2026-03-06 00:37:32 +0700

    Minor fixes to logic and additional elements to webpages

--- weather-forecast/asset/data.js
+++ weather-forecast/asset/data.js
@@ -11,8 +11,11 @@ async function getData(url) {
             console.error(error.message);
     } 
 }
-async function displayWeather(code) {
+async function displayWeather(code,code0,code1,code2) {
     document.getElementById("current-weather").innerHTML = processWeatherCode(code);
+    document.getElementById("wFI-0").innerHTML = processWeatherCode(code0);
+    document.getElementById("wFI-1").innerHTML = processWeatherCode(code1);
+    document.getElementById("wFI-2").innerHTML = processWeatherCode(code2);
 }
 async function displayTemperature(tcode) {
     let color;
@@ -73,8 +76,8 @@ function processWeatherCode(wCode) {
 function processWarning(tempMin,tempMax,humidP,wCode,aqi) {
     let coldsurge, heatwave, humid, thunderstorm, pollution;
     if (tempMin <= 10) {coldsurge = 3;}
-    else if (tempMin <= 16) {coldsurge = 2;}
-    else if  (tempMin <= 20) {coldsurge = 1;}
+    else if (tempMin <= 14) {coldsurge = 2;}
+    else if  (tempMin <= 19) {coldsurge = 1;}
     else {coldsurge = 0};
     if (tempMax >= 39) {heatwave = 3;}
     else if (tempMax >= 36) {heatwave = 2;}
@@ -98,33 +101,56 @@ function displayWarning(code) {
     for (let i=0; i<5; i++){
         let elementId = element[i];
         switch (true){
-            case code[i] == 1 : document.getElementById(elementId).style.backgroundColor = "#FFFF00"; break;
-            case code[i] == 2 : document.getElementById(elementId).style.backgroundColor = "#FF7F00"; break;
-            case code[i] == 3 : document.getElementById(elementId).style.backgroundColor = "#FF2C2C"; break;
+            case code[i] == 1 : document.getElementById(elementId).innerText = "🟨"+element[i]+" "; break;
+            case code[i] == 2 : document.getElementById(elementId).innerText = "🟧"+element[i]+" "; break;
+            case code[i] == 3 : document.getElementById(elementId).innerText = "🟥"+element[i]+" "; break;
             case code[i] == 0 : document.getElementById(elementId).style.display = "none"; break;
         }
     }
     let overall = Math.max(...code)
     switch (true) {
-            case overall == 1 : document.getElementById("overall").style.backgroundColor = "#FFFF00"; break;
-            case overall == 2 : document.getElementById("overall").style.backgroundColor = "#FF7F00"; break;
-            case overall == 3 : document.getElementById("overall").style.backgroundColor = "#FF2C2C"; break;
-            case overall == 0 : document.getElementById("overall").style.display = "none"; break;
+            case overall == 1 : document.getElementById("overall").innerText = "🟨"; break;
+            case overall == 2 : document.getElementById("overall").innerText = "🟧"; break;
+            case overall == 3 : document.getElementById("overall").innerText = "🟥"; break;
+            case overall == 0 : document.getElementById("overall").innerText = ": none"; break;
     }
 }
+function forecastTemperature(t) {
+    for (let i=0;i<6;i++ ) {
+        t[i] = t[i].toFixed(0);
+    }
+    console.log(t);
+    document.getElementById("wFT-0").innerHTML = t[0]+"-"+t[3]+"°C";
+    document.getElementById("wFT-1").innerHTML = t[1]+"-"+t[4]+"°C";
+    document.getElementById("wFT-2").innerHTML = t[2]+"-"+t[5]+"°C";
+}
+function displayDate(date) {
+    document.getElementById("wFD-0").innerText = date[0];
+    document.getElementById("wFD-1").innerText = date[1];
+    document.getElementById("wFD-2").innerText = date[2];
+}
 async function main() {
-    let url1 = "https://api.open-meteo.com/v1/forecast?latitude=21&longitude=105.77&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max,relative_humidity_2m_max&models=ecmwf_ifs&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code,precipitation&timezone=Asia%2FBangkok&forecast_days=3";
+    //retrive data from APIs
+    let url1 = "https://api.open-meteo.com/v1/forecast?latitude=20.98&longitude=105.8&daily=weather_code,temperature_2m_max,temperature_2m_min,relative_humidity_2m_mean,precipitation_probability_mean&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code,precipitation&timezone=Asia%2FBangkok&forecast_days=3";
     let url2 = "https://api.waqi.info/feed/here/?token=3c909ae15fd4b8726813b84233b6038cc95291ef";
     let meteo = await getData(url1);
     let aq = await getData(url2);
     console.log(meteo);
     console.log(aq);
+    //display current info
     document.getElementById("current-temp").innerHTML = meteo.current.temperature_2m;
-    document.getElementById("current-temp-wrap").style.color = await displayTemperature(meteo.current.temperature_2m);
+    document.getElementById("current-temp-wrap").style.backgroundColor = await displayTemperature(meteo.current.temperature_2m);
     document.getElementById("current-humid").innerHTML = meteo.current.relative_humidity_2m;
     document.getElementById("current-aqi").innerHTML = aq.data.aqi;
-    displayWeather(meteo.current.weather_code);
-    const warningCode = processWarning(meteo.daily.temperature_2m_min[0],meteo.daily.temperature_2m_max[0],meteo.daily.relative_humidity_2m_max[0],meteo.daily.weather_code[0],aq.data.aqi);
+    //display weather icon on all elements
+    displayWeather(meteo.current.weather_code,meteo.daily.weather_code[0],meteo.daily.weather_code[1],meteo.daily.weather_code[2]);
+    //display current weather warnings
+    const warningCode = processWarning(meteo.daily.temperature_2m_min[0],meteo.daily.temperature_2m_max[0],meteo.daily.relative_humidity_2m_mean[0],meteo.daily.weather_code[0],aq.data.aqi);
     displayWarning(warningCode);
+    //display forecast date
+    displayDate(meteo.daily.time)
+    //display temperature forecast
+    let tForecast = [meteo.daily.temperature_2m_min[0],meteo.daily.temperature_2m_min[1],meteo.daily.temperature_2m_min[2],meteo.daily.temperature_2m_max[0],meteo.daily.temperature_2m_max[1],meteo.daily.temperature_2m_max[2]] 
+    forecastTemperature(tForecast)
 }
 main();
