8a5ef053916800ba9a1063dcb6f4a56a71916d43
tthjose <138036385+tthjose@users.noreply.github.com>
2026-06-04 00:13:06 +0700

    code cleanup, UI update and minor change

---
+++ weather-forecast/asset/script/loading.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',function()
+{
+    document.querySelector("#loading-screen").style.display = "none";
+    document.querySelector("body").style.overflow = "visible";
+})
